a24ee3073d593ee8a592203ad0e880fd5785ea3a
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-19 14:53:35 -0600

    create fully functional back-end server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
+const messageCtrl = require('./controllers/message_controller')
 const SERVER_PORT = 3001
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
 
 //endpoints
-//get READ
-//post CREATE
-//put UPDATE
-//delete DELETE
+app.post(`/api/messages`, messageCtrl.create);
+app.get(`/api/messages`, messageCtrl.read);
+app.put(`/api/messages/:id`, messageCtrl.update);
+app.delete(`/api/messages/:id`, messageCtrl.delete);
 
-app.listen(SERVER_PORT, () => console.log(`App is listening on por ${SERVER_PORT}`))
+app.listen(SERVER_PORT, () => console.log(`App is listening on port ${SERVER_PORT}`))
